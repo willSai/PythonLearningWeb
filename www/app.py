5ec13ad9f8d72578b2cbbfb63b83ba3617074485
HAO <willlifeyo@gmail.com>
2017-11-30 17:23:47 +0800

    [ADD] add new response ~/home/{home}

--- www/app.py
+++ www/app.py
@@ -8,10 +8,15 @@ from aiohttp import web
 def index(request):
     return web.Response(body=b'<h1>Hi, Qian</h1>', content_type='text/html')
 
+def hello(request):
+    text = '<h1>hello, %s!</h1>' % request.match_info['name']
+    return web.Response(body=text.encode('utf-8'), content_type='text/html')
+
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
+    app.router.add_route('GET', '/home/{name}', hello)
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9306)
     logging.info('server started at http://127.0.0.1:9306...')
     return srv
